dd2a94e33af15fc9b2b17b785e820bbb66a59c6e
JrGustavo <gus@soypetal.com>
2023-05-13 19:05:59 -0500

    Course repaso javaScript

---
+++ src/bases/01-const.let.js
@@ -0,0 +1,19 @@
+// Variables y Constantes
+
+const nombre = 'Gustavo';
+const apellido = 'Alvarado';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado)
+
+//Var no se debe de usar
+
+if ( true )  {
+    const nombre = 'Peter'
+    console.log(valorDado)
+}
+
+console.log(valorDado);
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const nombre = 'Gustavo';
+const apellido = 'Herrera';
+
+//const nombreCompleto = nombre + ' ' + apellido;
+const nombreCompleto = `${nombre } ${apellido}`;
+
+console.log(nombreCompleto);
+
